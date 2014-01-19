3216b4786ebda50a4e7c971c83622661db8e837f
Nicolas Crowell <ncrowell@princeton.edu>
2014-01-19 08:11:24 -0800

    maclearn = igmp snooping

--- igmp_snoop.py
+++ igmp_snoop.py
@@ -1,46 +1,77 @@
-from pyretic.lib.corelib import if_
-from pyretic.lib.std import fwd, passthrough
-from pyretic.lib.query import match, DynamicPolicy
+from pyretic.lib.corelib import *
+from pyretic.lib.std import *
+from pyretic.lib.query import *
+from pyretic.modules.mac_learner import mac_learner
 
-from pox.lib.packet.packet import Packet
+from pox.lib.packet.igmp import igmp as Igmp
 
+import logging as logger
 
 # IP protocol number
 IGMP_PROTO = 2
 
-# we create a new dynamic policy class with the name "act_like_switch"
-class igmp_snoop(DynamicPolicy):
+class igmp_snoop_forward(DynamicPolicy):
     """
-    Implement learning-switch-like behavior.
+    Implement IGMP snooping for the topology's switches.
     """
-    # Pyretic predicates and policies are defined and documented in
-    # pyretic/core/language.py
+    # and policies - all of which are defined and documented in pyretic/core/language.py
     def __init__(self):
-        self.join_filter = match(protocol=IGMP_PROTO)
-        self.query = packets()
+        self.query = match(protocol=IGMP_PROTO) >> packets()
+        # keep track of group forwarding rules - need targeted deletion to
+        # handle 
+        self.group_rules = set()
 
-        self.joins = []
+        def track_group_membership(pkt):
+            try:
+                igmp = igmp_from_eth(pkt)
+            except Exception as e:
+                logger.exception(e)
 
-        self.multi_filter = match(dstip=MULTICAST_CIDR)
+            # Rule: if IP packet at this switch with this multicast address,
+            # then forward to the host which sent the join message
+            group_fwd = match(switch=pkt['switch'], dstip=igmp.address) >>
+                        fwd(pkt['port'])
+            # matching on IP dest on layer 2 because we can.
 
-        def track_joins(pkt):
-            print(pkt)
-            self.joins.append(pkt)
-            sys.exit(0)
+            if igmp.ver_and_type in (Igmp.MEMBERSHIP_REPORT,
+                                     Igmp.MEMBERSHIP_REPORT_V2):
+                group_rules.add(group_fwd)
+            elif igmp.ver_and_type == Igmp.LEAVE_GROUP_V2:
+                try: 
+                    group_rules.remove(group_fwd)
+                except KeyError as e:
+                    logger.exception(e)
+            else: # uninteresting IGMP message
+                pass
 
-            # update the dynamic policy to forward and query
-            # (each dynamic policy has a member 'policy'. whenever this member
-            # is assigned, the dynamic policy updates itself)
-            self.policy = self.forward + self.query
+            self.policy = union(group_rules) + self.query
 
-        # learn_from_a_packet is called back every time query sees a new packet
-        self.query.register_callback(learn_from_a_packet)
+        self.query.register_callback(track_group_membership)
+        super(igmp_snoop_forward, self).__init__(self.query)
 
-        # finally, initialize the dynamic policy 
-        super(act_like_switch,self).__init__(self.forward + self.query)
 
+def eth_payload(packet):
+    # mostly taken from pyretic example "dpi.py"
+    eth_bytes = [ord(b) for b in packet['raw']]
+    return eth_bytes[packet['header_len']:]
+
+WORDS_TO_BYTES = 4
+def ip_payload(packet):
+    ethPayload = eth_payload(packet)
+    #ip_version = (ethPayload[0] & 0b11110000) >> 4
+    #assert ip_version == 4 or ip_version == 127
+    #ip_proto   = ethPayload[9]
+    ihl = (ethPayload[0] & 0b00001111)
+    ip_header_len = ihl * WORDS_TO_BYTES
+    return ethPayload[ip_header_len:]
+
+def igmp_from_eth(rawpacket):
+    ipPayload = ip_payload(rawpacket)
+    return Igmp(raw=ipPayload)
 
 def main():
-    ## The main method returns the policy that will be run  
-    return act_like_switch()
+    # policy when this module is run as the main policy
+    return igmp_snoop_forward() + mac_learner()
 
+if __name__ == '__main__'
+    main()
